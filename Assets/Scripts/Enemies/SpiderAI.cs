19f572282c2545a74b2301ccd9636872301e8d17
koyle <kylerowe@rogers.com>
2018-02-02 08:56:04 -0500

    some enemies

--- Assets/Scripts/Enemies/SpiderAI.cs
+++ Assets/Scripts/Enemies/SpiderAI.cs
@@ -7,7 +7,7 @@ public class SpiderAI : MonoBehaviour
 {
     NavMeshAgent agent;
 
-    public Transform[] Wanderpoints = new Transform[10];
+    public Transform[] Wanderpoints = new Transform[4];
 
 	// Use this for initialization
 	void Start ()
@@ -18,7 +18,7 @@ public class SpiderAI : MonoBehaviour
 
     IEnumerator NewWanderPoint()
     {
-        agent.SetDestination(Wanderpoints[Random.Range(0, 10)].position); //pick random wander point
+        agent.SetDestination(Wanderpoints[Random.Range(0, 4)].position); //pick random wander point
         Debug.Log("wandering.. waiting");
         yield return new WaitForSeconds(Random.Range(6, 13)); //wait 6-12s
         Debug.Log("done waiting");
