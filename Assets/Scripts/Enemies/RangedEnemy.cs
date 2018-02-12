99c51e6de989373a832201f4cfb539864f7cdb77
Kyle Rowe <kylerowe@Kyles-MacBook-Pro.local>
2018-02-12 11:00:39 -0500

    ranged enemy projectile

--- Assets/Scripts/Enemies/RangedEnemy.cs
+++ Assets/Scripts/Enemies/RangedEnemy.cs
@@ -1,24 +1,25 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RangedEnemy : MonoBehaviour
 {
-    float minDamage = 1; //random numbers for now
-    float maxDamage = 5;
-    float scaleDamage;
+    float damage = 5;
 
     private SphereCollider range;
     private Transform player;
     private Vector3 playerLocation;
     private bool shooting = false;
 
+    public GameObject projectile;
+    public Transform projectileSpawn;
+
     // Use this for initialization
     void Start()
     {
         range = GetComponent<SphereCollider>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        scaleDamage = maxDamage - minDamage;
+        //scaleDamage = maxDamage - minDamage;
     }
 
     void Update()
@@ -32,10 +33,10 @@ public class RangedEnemy : MonoBehaviour
         }
     }
 
-    void FaceTarget() //hard to tell without real models but i think its buggy? it follows the player perfectly just not aligned properly
+    void FaceTarget()
     {
         Vector3 direction = (player.position - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, direction.y, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 30f);
     }
 
@@ -44,16 +45,16 @@ public class RangedEnemy : MonoBehaviour
         Debug.Log("shoot");
         shooting = true;
         StartCoroutine("Cooldown");
-        //play some gun fire effect? or does it shoot a projectile?
-        float fractionalDistance = (range.radius - Vector3.Distance(transform.position, player.position)) / range.radius; //if player is standing on top of enemy this = 1, if player is at max range from enemy this = 0
-        float damage = scaleDamage * fractionalDistance + minDamage; //if standing on enemy, 4 * 1 + 1 = 5 aka max damage. if at max range, 4 * 0 + 1 = 1 aka min damage
-        HealthManager.instance.health -= damage;
+        GameObject bullet = Instantiate(projectile, projectileSpawn.transform.position, projectileSpawn.transform.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 20;
+        //float fractionalDistance = (range.radius - Vector3.Distance(transform.position, player.position)) / range.radius; //if player is standing on top of enemy this = 1, if player is at max range from enemy this = 0
+        //float damage = scaleDamage * fractionalDistance + minDamage; //if standing on enemy, 4 * 1 + 1 = 5 aka max damage. if at max range, 4 * 0 + 1 = 1 aka min damage
     }
 
     IEnumerator Cooldown()
     {
         Debug.Log("start cooldown");
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(3);
         Debug.Log("end cooldown");
         shooting = false;
     }
