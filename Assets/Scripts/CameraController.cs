c7986b43a68fa5121940d08975d29539ef19395f
Socrates <socratesmridha@gmail.com>
2018-02-16 11:07:26 -0500

    Updated the CameraController, added a Camera Type Pefab and Script.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,8 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 //Written by: Socrates
-//Last Updated : 26/01/2018
+//Start Date: Who Knows?
+//Last Updated: 16/02/2018
+//Finished Date: 
 
+/*
+ Camera Settings:
+*All positions are player transform plus the veriables below (x,y,z)*
+
+Wide Screen:(For Testing) 0
+position(4,6.5,-20)
+rotation(15,0,0)
+scale(1,1,1)
+
+Default: 1
+position(4,6.5,-10)
+rotation(15,0,0)
+scale(1,1,1)
+
+Angled: 2
+position(2,6.5,-10)
+rotation(15,25,0)
+scale(1,1,1)
+
+Fixed: 3
+position(10,6.5,-15)
+rotation(15,0,0)
+scale(1,1,1)
+*/
 
 public class CameraController : MonoBehaviour
 {
@@ -12,41 +38,124 @@ public class CameraController : MonoBehaviour
 
 
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    Vector3 rOffset;
+
+    //Position
+    Vector3 pDefault = new Vector3(4, 4, -10);
+    Vector3 pWideScreen = new Vector3(4, 4, -20);
+    Vector3 pAngled = new Vector3(2, 4, -10);
+    Vector3 pFixed = new Vector3(10, 4, -15);
+
+    //Rotation
+    Vector3 rDefault = new Vector3(15, 0, 0);
+    Vector3 rWideScreen = new Vector3(15, 0, 0);
+    Vector3 rAngled = new Vector3(15, 25, 0);
+    Vector3 rFixed = new Vector3(15, 0, 0);
+
+    public enum Cam {WideScreen,Default,Angled,Fixed};
+    public int camNum;
+
+    Cam PlayerCam;
+    Cam SetCam;
+
     bool zoom;
+
     // Use this for initialization
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
-
         
+        //offset = pDefault - player.transform.position;
+        offset = pDefault;
+
+        //rotation
+        transform.rotation = Quaternion.Euler(15, 0, 0);
+
+        PlayerCam = Cam.Default;
+        SetCam = PlayerCam;
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        if (zoom)
+        
+        transform.position = player.transform.position + offset;
+        //transform.Rotate(rDefault);
+
+        if (camNum == 0)
         {
-            transform.position = player.transform.position + (offset);
+            PlayerCam = Cam.WideScreen;
+        }
+        else if (camNum == 1)
+        {
+            PlayerCam = Cam.Default;
+        }
+        else if (camNum == 2)
+        {
+            PlayerCam = Cam.Angled;
+        }
+        else if (camNum == 3)
+        {
+            PlayerCam = Cam.Fixed;
         }
 
-        if(!zoom)
+        if (SetCam != PlayerCam)
         {
-            ZoomIn();
+            CameraType(PlayerCam);
+            SetCam = PlayerCam;
         }
 
-        //for testing 
-        if (Input.GetKeyDown(KeyCode.Z))
+        //For TESTING***
+        if (Input.GetKeyDown(KeyCode.Alpha0))
         {
-            zoom = !zoom;
+            PlayerCam = Cam.WideScreen;
         }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PlayerCam = Cam.Default;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PlayerCam = Cam.Angled;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PlayerCam = Cam.Fixed;
+        }
+        //For TESTING***
+
     }
-    void ZoomIn()
+    void CameraType(Cam type)
     {
-        transform.position = player.transform.position + (offset + new Vector3(0, 0, 0));
+        switch (type)
+        {
+            case Cam.WideScreen:
+                offset = pWideScreen ;
+                transform.rotation = Quaternion.Euler(15, 0, 0);
+                break;
+
+            case Cam.Default:
+                offset = pDefault ;
+                transform.rotation = Quaternion.Euler(15, 0, 0);
+                break;
+
+            case Cam.Angled:
+                offset = pAngled ;
+                transform.rotation =  Quaternion.Euler(15, 25, 0);
+                break;
+
+            case Cam.Fixed:
+                offset = pFixed ;
+                transform.rotation = Quaternion.Euler(15, 0, 0);
+                break;
+
+
+        }
     }
+   
+    
     
 
 }
---
+++ Assets/Scripts/ChangeCameraAngle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCameraAngle : MonoBehaviour {
+
+    [SerializeField]
+    bool Angled;
+    [SerializeField]
+    bool Fixed;
+
+
+    GameObject MainCam;
+    CameraController Cam;
+
+    // Use this for initialization
+    void Start () {
+        MainCam = GameObject.Find("Main Camera");
+        Cam = MainCam.GetComponent<CameraController>();
+        Cam.camNum = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (Angled)
+            {
+                Cam.camNum = 2;
+            }
+            else if (Fixed)
+            {
+                Cam.camNum = 3;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Cam.camNum = 1;
+        }
+    }
+}
