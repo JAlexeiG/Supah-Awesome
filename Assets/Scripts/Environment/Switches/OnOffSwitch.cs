9bc4687f601fbbad5ba7a77e1127c436620fa596
KingTimmah <tim-kwon-do@hotmail.ca>
2018-05-04 03:27:04 -0400

    floating platform update + timer switch + on/off switch

---
+++ Assets/Scripts/Environment/Switches/OnOffSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffSwitch : Power {
+
+    public GameObject[] pow;
+    private bool switchOn;
+    // Use this for initialization
+    void Start()
+    {
+        switchOn = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player") //add && melee input
+        {
+            foreach (GameObject i in pow)
+            {
+                i.GetComponent<Power>().PowerSwitch();
+            }
+        }
+    }
+}
