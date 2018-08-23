d314f19af039a696d5703c2ca6887c472b1a30d3
JAlexeiG <angelbloodriver1@gmail.com>
2018-08-23 13:51:15 -0400

    Changed stuff in my scene $ also Chara script

---
+++ Assets/ZZZ DO NOT TOUCJH/Test Scripts/BodyRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyRotation : MonoBehaviour {
+
+    public GameObject spineRotator;
+    public GameObject lookAt;
+
+    public Vector3 mousePos;
+    public float cameraPos;
+	
+	// Update is called once per frame
+	void Update () {
+        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z + cameraPos);
+
+
+
+        Vector3 potato = Camera.main.ScreenToWorldPoint(mousePos);
+
+        potato = new Vector3(potato.x, potato.y, 0);
+
+        Debug.Log(potato);
+
+        lookAt.transform.position = potato;
+
+        spineRotator.transform.LookAt(lookAt.transform);
+
+	}
+}
